8fb4fc0c7f052ccb31c186e4aad9f626ecc9dac0
Travis Young <travis.young@tenexsolutions.com>
2023-02-21 17:58:41 -0500

    Initial Commit

---
+++ CoinFlip Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinFlip Project
+//
+//  Created by Travis Young on 2/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
